aeb4fe6cc5b7b1727a1d27f5f9977ff46434ae88
Viktoriia Melnyk <melnychka1993@gmail.com>
2025-08-29 17:41:18 +0100

    changed path for deploy

--- src/Root.jsx
+++ src/Root.jsx
@@ -7,10 +7,15 @@ import { PrivacyPage } from './pages/PrivacyPage';
 import { TermsPage } from './pages/TermsPage';
 import { DisclosurePage } from './pages/DisclosurePage';
 
+export const BASE_URL =
+	window.location.hostname === 'localhost'
+		? ''
+		: '/blood_device_land_portfolio';
+
 export const Root = () => (
 	<BrowserRouter>
 		<Routes>
-			<Route path={'/'} element={<App />}>
+			<Route path={`${BASE_URL}/`} element={<App />}>
 				<Route index element={<HomePage />} />
 				<Route path="about" element={<AboutPage />} />
 				<Route path="contact" element={<ContactPage />} />
